c321c7c7da6291582a002b9486e330fee74186fa
taylormonacelli <tailor@u.washington.edu>
2014-07-25 19:58:39 -0700

    yada-yada

--- getone.sh
+++ getone.sh
@@ -1,4 +1,10 @@
-fbase=$(make lhm | awk '{print $(NF)}' | gshuf | head -1 \
-    | sed -e 's,_th$,,')
+fbase=""
+
+while test -z "$fbase"
+do
+    fbase=$(make lhm | awk '{print $(NF)}' | gshuf | head -1 \
+	| sed -e 's,_th$,,')
+done
+
 grep $fbase current
 grep $fbase calc
